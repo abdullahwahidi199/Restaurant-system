217866a687c78dcd28d1992721054e625d702173
abdullahwahidi199 <wahidia53@gmail.com>
2026-05-05 14:58:49 +0430

    db altered

--- backend/backend/backend/settings.py
+++ backend/backend/backend/settings.py
@@ -113,26 +113,26 @@ CHANNEL_LAYERS = {
         },
     },
 }
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'rms_db',
-#         'USER':'rms_user',
-#         'PASSWORD':'AbdullahWahidi123',
-#         'HOST':'localhost',
-#         'PORT':'5432'
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'rms_db',
+        'USER':'rms_user',
+        'PASSWORD':'AbdullahWahidi123',
+        'HOST':'localhost',
+        'PORT':'5432'
+    }
+}
 
 RATELIMIT_IP_META_KEY = "HTTP_X_REAL_IP"
 
 BASE_URL = "http://185.197.249.94"
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 from datetime import timedelta
