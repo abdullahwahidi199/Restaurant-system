fbeb5994f0971ea3d5d7890a66f7435fbe9249d4
abdullahwahidi199 <wahidia53@gmail.com>
2026-05-17 05:04:08 +0430

    table chnage feature added

--- frontend/src/hooks/useDiscoutSocket.js
+++ frontend/src/hooks/useDiscoutSocket.js
@@ -12,7 +12,7 @@ export default function useDiscountSocket(onMessage) {
     if (socketRef.current) return; // prevent duplicate connections
 
     const socket = new WebSocket(
-      `ws://127.0.0.1:8000/ws/discounts/${restaurantId}/`,
+      `ws://185.197.249.94/ws/discounts/${restaurantId}/`,
     );
 
     socketRef.current = socket;
